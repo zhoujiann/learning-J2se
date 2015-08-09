820a0727ec3e074b49fd78011722f339126b2857
zhoujann <zhoujan_0106@126.com>
2015-08-09 22:14:37 +0800

    TreeSet添加自定义对象

--- Git_1508/src/com/zj/demo/treeset/Demo002.java
+++ Git_1508/src/com/zj/demo/treeset/Demo002.java
@@ -1,9 +1,27 @@
 package com.zj.demo.treeset;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import com.zj.demo.utils.Utils;
+
 public class Demo002 {
 	public static void main(String[] args) {
 		/*
 		 * set中添加自定义对象
 		 */
+		TreeSet<Person> ts = new TreeSet<Person>() ;
+		/*
+		 * 如果像下面这样做
+		 * Exception in thread "main" java.lang.ClassCastException:
+		 * com.zj.demo.treeset.Person cannot be cast to java.lang.Comparable
+		 * -->所以，了解Comparable
+		 */
+//		ts.add(new Person("张三", 23));
+		
+		for (Iterator<Person> it = ts.iterator(); it.hasNext() ;) {
+			Utils.sop(it.next());
+		}
+		
 	}
 }
--- Git_1508/src/com/zj/demo/treeset/Person.java
+++ Git_1508/src/com/zj/demo/treeset/Person.java
@@ -5,7 +5,7 @@ import com.zj.demo.utils.Utils;
 public class Person {
 	private String name ;
 	private int age ;
-	public Person(String name, int age) {
+	Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}

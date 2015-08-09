71162d48c91fd44f8ea6e7a4723e7bf4057bf95c
zhoujann <zhoujan_0106@126.com>
2015-08-09 21:42:04 +0800

    TreeSet的排序：

--- Git_1508/src/com/zj/demo/treeset/Demo001.java
+++ Git_1508/src/com/zj/demo/treeset/Demo001.java
@@ -1,5 +1,23 @@
 package com.zj.demo.treeset;
 
-public class Demo001 {
+import java.util.Iterator;
+import java.util.TreeSet;
 
+public class Demo001 {
+	// learning treeset
+	public static void main(String[] args) {
+		TreeSet<String> tr = new TreeSet<String>();
+		tr.add("abcd");
+		tr.add("bcda");
+		tr.add("bcad");
+		tr.add("Abcd");
+		for (Iterator<String> it = tr.iterator() ; it.hasNext() ;) {
+			sop(it.next());
+		}
+	}
+	
+	public static void sop(Object obj){
+		System.out.println(obj);
+	}
+	
 }

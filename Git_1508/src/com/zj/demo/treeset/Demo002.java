92cf62c93d91f65f8799801d6a1c55243c9d405e
zhoujann <zhoujan_0106@126.com>
2015-08-09 22:05:59 +0800

    TreeSet中添加自定义对象

---
+++ Git_1508/src/com/zj/demo/treeset/Demo002.java
@@ -0,0 +1,9 @@
+package com.zj.demo.treeset;
+
+public class Demo002 {
+	public static void main(String[] args) {
+		/*
+		 * set中添加自定义对象
+		 */
+	}
+}
---
+++ Git_1508/src/com/zj/demo/treeset/Person.java
@@ -0,0 +1,18 @@
+package com.zj.demo.treeset;
+
+import com.zj.demo.utils.Utils;
+
+public class Person {
+	private String name ;
+	private int age ;
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	void shout(){
+		Utils.sop(name + " ½ñÄê " + age + " ËêÁË ~");
+	}
+	
+	
+}

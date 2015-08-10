e63abd4b82776bf93f40b9efa9426d8c008a7e11
zhoujann <zhoujan_0106@126.com>
2015-08-10 23:19:00 +0800

    TreeSet的CompareTo方法的初步使用

--- Git_1508/src/com/zj/demo/treeset/Demo002.java
+++ Git_1508/src/com/zj/demo/treeset/Demo002.java
@@ -3,25 +3,41 @@ package com.zj.demo.treeset;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-import com.zj.demo.utils.Utils;
 
 public class Demo002 {
 	public static void main(String[] args) {
-		/*
-		 * set中添加自定义对象
-		 */
+		 // set中添加自定义对象
 		TreeSet<Person> ts = new TreeSet<Person>() ;
 		/*
 		 * 如果像下面这样做
 		 * Exception in thread "main" java.lang.ClassCastException:
 		 * 		com.zj.demo.treeset.Person cannot be cast to java.lang.Comparable
 		 * -->所以，了解Comparable
+		 * 
+		 * 
+		 *  张三.......CompareTo.......张三
+		 *	李四.......CompareTo.......张三
+		 *	王五.......CompareTo.......张三
+		 *	王五.......CompareTo.......李四
+		 *	李四 今年 21 岁了 ~
+		 *	王五 今年 23 岁了 ~
+		 *	张三 今年 25 岁了 ~
+		 *
+		 *输出结果和jdk1.6大为不同，1.7中，很明显的迭代形式的比较，
+		 * 第一行的输出就能很明显的说明为何只有一个自定义元素也不能够用ts迭代出来
+		 * 遍历比较
 		 */
-//		ts.add(new Person("张三", 23));
-		
-		for (Iterator<Person> it = ts.iterator(); it.hasNext() ;) {
-			Utils.sop(it.next());
+		ts.add(new Person("张三",25));
+		ts.add(new Person("李四",21));
+		ts.add(new Person("王五",23));
+//		for (Iterator<Person> it = ts.iterator(); it.hasNext() ;) {
+//			it.next().shout();
+//		}
+		Iterator<Person> it = ts.iterator() ;
+		while(it.hasNext()){
+			it.next();
 		}
+		
 		
 	}
 }
--- Git_1508/src/com/zj/demo/treeset/Person.java
+++ Git_1508/src/com/zj/demo/treeset/Person.java
@@ -2,7 +2,7 @@ package com.zj.demo.treeset;
 
 import com.zj.demo.utils.Utils;
 
-public class Person {
+public class Person implements Comparable<Object>{
 	private String name ;
 	private int age ;
 	Person(String name, int age) {
@@ -13,6 +13,26 @@ public class Person {
 	void shout(){
 		Utils.sop(name + " 今年 " + age + " 岁了 ~");
 	}
+
+	@Override
+	public int compareTo(Object obj) {
+		if (!(obj instanceof Person))
+			throw new RuntimeException("is not a Person Obj");
+		Person person = (Person) obj ;
+		/*
+		 * 为什么和毕向东的比较顺序不一样，是因为jdk的原因吗
+		 * 此外，jdk1.7即使放入单个自定义对象，也不能够输出了
+		 */
+		Utils.sop(this.name + ".......CompareTo......." + person.name);
+		if (this.age > person.age)
+		return 11;
+		
+		if(this.age == person.age)
+			return 0 ;
+		return -1 ;
+		
+	}
+	
 	
 	
 }
